61a2dfb06fb311d3230ec4ff3a390812ef99916a
MVC <michael.colianna@me.com>
2020-05-03 00:28:22 -0400

    Updates Hamilton theme to 2.0.2

--- public/wp-content/themes/hamilton-child/functions.php
+++ public/wp-content/themes/hamilton-child/functions.php
@@ -4,11 +4,4 @@ add_action( 'wp_enqueue_scripts', 'hamilton_child_enqueue_styles' );
 function hamilton_child_enqueue_styles()
 {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_script( 'render-block', get_stylesheet_directory_uri() . '/render-block.js', ['jquery']  );
-}
-
-add_action( 'init', 'remove_hamilton_has_js' );
-function remove_hamilton_has_js()
-{
-    remove_action( 'wp_head', 'hamilton_has_js' );
 }
