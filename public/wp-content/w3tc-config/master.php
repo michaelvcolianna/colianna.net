72caeb21f612fa5e8626942046102c77a755c15a
MVC <michael.colianna@me.com>
2021-04-10 16:22:18 -0400

    Updates W3TC plugin

--- public/wp-content/w3tc-config/master.php
+++ public/wp-content/w3tc-config/master.php
@@ -1,5 +1,5 @@
 <?php exit; ?>{
-    "version": "2.1.1",
+    "version": "2.1.2",
     "cluster.messagebus.debug": false,
     "cluster.messagebus.enabled": false,
     "cluster.messagebus.sns.region": "",
@@ -58,6 +58,7 @@
         "\\bsql_calc_found_rows\\b",
         "\\bfound_rows\\(\\)"
     ],
+    "docroot_fix.enable": false,
     "lazyload.enabled": false,
     "lazyload.process_img": true,
     "lazyload.process_background": true,
