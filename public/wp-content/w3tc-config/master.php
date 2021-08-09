2c1d3802c4a6d6aa6caca38aad21c6c47f303834
MVC <michael.colianna@me.com>
2021-08-08 21:42:29 -0400

    Updates WordPress to 5.8, updates W3TC plugin, updates built-in themes

--- public/wp-content/w3tc-config/master.php
+++ public/wp-content/w3tc-config/master.php
@@ -1,5 +1,5 @@
 <?php exit; ?>{
-    "version": "2.1.5",
+    "version": "2.1.6",
     "cluster.messagebus.debug": false,
     "cluster.messagebus.enabled": false,
     "cluster.messagebus.sns.region": "",
